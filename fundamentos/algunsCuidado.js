0e3adab5c479564d2615550493a5685c5f51d19d
RonalddMatias <ronaldd.feliph.matias.costa@ccc.ufcg.edu.br>
2023-02-09 21:46:29 -0300

    strings

--- fundamentos/algunsCuidado.js
+++ fundamentos/algunsCuidado.js
@@ -3,3 +3,5 @@ console.log("10"/2)
 console.log("show" * 2)
 console.log(0.1 + 0.7) //imprecisao pq o js suporta até determinada quantidade 
 console.log(10.23.toFixed(1))
+console.log("3" + 2)
+console.log("2" - 2)
---
+++ fundamentos/string.js
@@ -0,0 +1,10 @@
+const escola = "UFCG"
+
+// quando não tem o índece na string ele simplesmente printa algo vazio
+console.log(escola.charAt(2))
+console.log(escola.charCodeAt(3))
+console.log(escola.substring(1)) // do indice 1 em diante
+console.log("escola" + escola + "!")
+console.log(escola.replace("U", 2))
+
+console.log('ana,maria,ronaldd'.split(",")) // transformando em array
