ca461d4235d99a6a88956fe606764f1c981759ff
RonalddMatias <ronaldd.feliph.matias.costa@ccc.ufcg.edu.br>
2023-02-14 15:12:57 -0300

    templateString e Booleanos

---
+++ fundamentos/booleanos.js
@@ -0,0 +1,9 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+let nome = 'lucas'
+
+console.log(nome || 'desconhecido') // caso nome não estaja válido, "desconhecido será o padrão"
---
+++ fundamentos/templateString.js
@@ -0,0 +1,12 @@
+const nome = "Ronaldd"
+const concantenacao = "olá" + nome + "!"
+
+const template = `olá ${nome}!`
+
+console.log(template)
+
+// expressoes
+
+console.log(`1 + 1 = ${1+1}`)
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('ronaldd')}!`)
